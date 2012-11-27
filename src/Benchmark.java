b4f2a087f0ac28b8f585d88992a7cd28238f363a
Kevin Chen <kevinwchen26@gmail.com>
2012-11-27 17:40:40 -0600

    added benchmarking and testing file

---
+++ src/Benchmark.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kevin
+ * Date: 11/27/12
+ * Time: 4:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Benchmark {
+}
